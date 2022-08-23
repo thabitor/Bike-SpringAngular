dc4a2eb0d8c45185420bbb8f4d9380d9f4284141
thabitor <thabithamror@gmail.com>
2022-08-23 15:13:27 +0200

    initial commit

--- src/main/java/com/globomatics/bike/BikeApplication.java
+++ src/main/java/com/globomatics/bike/BikeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BikeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BikeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BikeApplication.class, args);
+    }
 
 }
